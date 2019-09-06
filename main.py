fd73b605d63f2f5f5ae4acbd6ff738e296569b22
stygianw <stygianwatcher@gmail.com>
2019-09-06 20:42:18 +0300

    nested ORFs with test

--- main.py
+++ main.py
@@ -17,72 +17,53 @@ def read_file(filename):
                 result[key] = result[key] + line
     return result
             
-
-def find_longest_orf_in_reading_2():
-    data = read_file('dna2.fasta')
-    lengths = []
-    for sequence in data.values():
-        forward = find_all_orfs_in_reading(sequence, 1)
-        backward = find_all_orfs_in_reading(''.join(reversed(sequence)), 1)
-        lengths.extend([len(x[1]) for x in forward])
-        lengths.extend([len(x[1]) for x in backward])
-    return max(lengths)
-    
-
-def any_sequence_any_forward_longest_length():
-    data = read_file('dna2.fasta')
-    all_results = []
-    for sequence in data.values():
-        reading1 = find_all_orfs_in_reading(sequence, 0)
-        reading2 = find_all_orfs_in_reading(sequence, 1)
-        reading3 = find_all_orfs_in_reading(sequence, 2)
-        all_results.extend(x for x in reading1)
-        all_results.extend(x for x in reading2)
-        all_results.extend(x for x in reading3)
-    longest_element = max(all_results, key=lambda x: len(x[1]))
-    return len(longest_element[1])
-
-
-def starting_position_in_longest_orf_reading3():
-    data = read_file('dna2.fasta')
-    all_results = []
-    for sequence in data.values():
-        forward = find_all_orfs_in_reading(sequence, 2)
-        backward = find_all_orfs_in_reading(''.join(reversed(sequence)), 2)
-        all_results.extend(x for x in forward)
-        all_results.extend(x for x in backward)
-    longest_element = max(all_results, key=lambda x: len(x[1]))
-    return longest_element[0] + 1
-
-def find_all_orfs_in_reading(sequence, offset = 0):
+            
+def find_all_orfs_in_reading(sequence, offset = 0, reverse = False):
     result_list = []
-    orf_started = False; orf_start_idx = None
-    current_orf = ''
+    if reverse:
+        sequence = ''.join(reversed(sequence))
+    current_segments = []
     for current_idx in range(offset, len(sequence), 3):
         portion = ''.join(sequence[current_idx:current_idx + 3])
-        if(portion == SEQ_START and not orf_started):
-            orf_start_idx = current_idx
-            orf_started = True
-        if orf_started:
-            current_orf += portion
-        if orf_started and portion in SEQ_END:
-            result_list.append((orf_start_idx, current_orf, sequence))
-            orf_start_idx = None
-            orf_started = False
-            current_orf = ''
+        if(portion == SEQ_START):
+            current_segments.append([current_idx + 1, ''])
+        if current_segments:
+            for current_segment in current_segments:
+                current_segment[1] += portion
+        if current_segments and portion in SEQ_END:
+            result_list.extend((x[0], x[1]) for x in current_segments)
+            current_segments.clear()
     return result_list
 
 
-print(any_sequence_any_forward_longest_length())
+if __name__ == '__main__':
+    print(any_sequence_any_forward_longest_length())
 
 
 def test_find_all_orfs_in_reading_reading2():
     result = find_all_orfs_in_reading('AATGAAGAACTAGGTAATGGAGTGAAATA', 1)
     assert len(result) == 2
-    assert result[0][0] == 1
+    assert result[0][0] == 2
     assert result[0][1] == "ATGAAGAACTAG"
-    assert result[1][0] == 16
+    assert result[1][0] == 17
     assert result[1][1] == "ATGGAGTGA"
+
+def test_find_all_orfs_in_reading_reading2_reversed():
+    result = find_all_orfs_in_reading('AATGAATAACTAGGTAATGGAGTGAAATA', 1, True)
+    assert len(result) == 1
+    assert result[0][0] == 14
+    assert result[0][1] == "ATGGATCAATAA"
+
+
+def test_find_all_orfs_in_reading_reading2_nested():
+    result = find_all_orfs_in_reading('AATGAAGTCAATGAACTAGGTAATGGAGTGAAATA', 1)
+    assert len(result) == 3
+    assert result[0][0] == 2
+    assert result[0][1] == "ATGAAGTCAATGAACTAG"
+    assert result[1][0] == 11
+    assert result[1][1] == "ATGAACTAG"
+    assert result[2][0] == 23
+    assert result[2][1] == "ATGGAGTGA"
 
 
 def test_find_all_orfs_in_reading_reading1_notfound():
